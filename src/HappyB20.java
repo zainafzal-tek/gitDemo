3f88d990df37830c2dc4ab9b8843b2ba34836c3f
zainafzal-tek <zainafzal@gmail.com>
2020-08-01 11:50:23 -0400

    update HappyB20.java

--- src/HappyB20.java
+++ src/HappyB20.java
@@ -3,6 +3,7 @@ public class HappyB20 {
     public static void main(String[] args) {
 
         System.out.println("B20 is always happy");
+        System.out.println("Hello B20, how are you doing today?");
 
     }
 
